aa9e9f78bb9ca8388ab5c5a6582a1778fc9f0627
LegendaryLeg <mrfunnymonkey2468@gmail.com>
2024-06-26 19:39:06 +0500

    Small changes

--- Controller.php
+++ Controller.php
@@ -1,5 +1,18 @@
 <?php
 
-require('ModelPHP.php')
+require('ModelPHP.php');
+
+if(empty($_POST["page"]))
+{
+    include("index.html");
+    exit();
+}
+
+if ($_POST['page'] == 'Start_Page')
+{
+    
+}
+
+
 
 ?>
--- ModelPHP.php
+++ ModelPHP.php
@@ -2,4 +2,16 @@
 
 $conn = new PDO("sqlsrv:server = tcp:primeempire.database.windows.net,1433; Database = PrimeEmpireDB", "primeadmin", "primeempire24!");
 
+function addUnit($name, $type, $weight, $imageurl)
+{
+    global $conn;
+    $sql = "insert into TeaTable values(null '$name', '$type' ,'$weight', '$imageurl')";
+
+    $result = mysqli_query($conn, $sql);
+
+    if ($result == true) 
+    {return true;}
+}
+
+
 ?>
