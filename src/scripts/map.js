b095fdefcb0f4a43ae0915d149a4482933ce50c9
daydreamer <nimandrey22@gmail.com>
2024-06-29 21:58:55 +0300

    upd 29.06.24
    Updated all pages

--- src/scripts/map.js
+++ src/scripts/map.js
@@ -1,21 +1,25 @@
 const map = new mapgl.Map('map-container', {
     key: 'c00db97a-a948-4548-9134-189200bb007b',
-    center: [38.09967742208324, 53.146523656903916],
-    zoom: 13,
+    center: [76.90079012364914, 43.25799707741974],
+    zoom: 20,
     style: 'c080bb6a-8134-4993-93a1-5b4d8c36a59b'
 });
 
-// const mapInfo = document.getElementById('map-info');
+const marker = new mapgl.Marker(map, {
+    coordinates: [76.90079012364914, 43.25799707741974],
+});
 
-// marker.on('mouseover', (e) => {
-//     const offset = 20;
+const mapInfo = document.getElementById('map-info');
 
-//     console.log(e.point);
-//     mapInfo.style.top = `${e.point[1] + offset}px`;
-//     mapInfo.style.left = `${e.point[0] + offset}px`;
-//     mapInfo.style.display = 'block';
-// })
+marker.on('mouseover', (e) => {
+    const offset = 20;
 
-// marker.on('mouseout', (e) => {
-//     mapInfo.style.display = 'none'
-// })
+    console.log(e.point);
+    mapInfo.style.top = `${e.point[1] + offset}px`;
+    mapInfo.style.left = `${e.point[0] + offset}px`;
+    mapInfo.style.display = 'block';
+})
+
+marker.on('mouseout', (e) => {
+    mapInfo.style.display = 'none'
+})
