8aa3ad0c377577cc6d0b1aa70b4837df85742c1b
daydreamer <nimandrey22@gmail.com>
2024-06-29 10:30:43 +0300

    Styles update 29.06.24

--- src/scripts/map.js
+++ src/scripts/map.js
@@ -1,35 +1,21 @@
-// const map = new mapgl.Map('map-container', {
-//     center: [76.900780, 43.258009],
-//     zoom: 20,
-//     key: 'c00db97a-a948-4548-9134-189200bb007b',
-// });
-
 const map = new mapgl.Map('map-container', {
     key: 'c00db97a-a948-4548-9134-189200bb007b',
-    center: [55.31878, 25.23584],
+    center: [38.09967742208324, 53.146523656903916],
     zoom: 13,
+    style: 'c080bb6a-8134-4993-93a1-5b4d8c36a59b'
 });
 
-const marker = new mapgl.Marker(map, {
-    coordinates: [76.900780, 43.258009],
-});
+// const mapInfo = document.getElementById('map-info');
 
-// const htmlMarker = new mapgl.HtmlMarker(map, {
-//     coordinates: map.getCenter(),
-//     html: '<div>Ауэзова 1а, офис 1 этаж 2</div>',
-// });
+// marker.on('mouseover', (e) => {
+//     const offset = 20;
 
-const mapInfo = document.getElementById('map-info');
+//     console.log(e.point);
+//     mapInfo.style.top = `${e.point[1] + offset}px`;
+//     mapInfo.style.left = `${e.point[0] + offset}px`;
+//     mapInfo.style.display = 'block';
+// })
 
-marker.on('mouseover', (e) => {
-    const offset = 20;
-
-    console.log(e.point);
-    mapInfo.style.top = `${e.point[1] + offset}px`;
-    mapInfo.style.left = `${e.point[0] + offset}px`;
-    mapInfo.style.display = 'block';
-})
-
-marker.on('mouseout', (e) => {
-    mapInfo.style.display = 'none'
-})
+// marker.on('mouseout', (e) => {
+//     mapInfo.style.display = 'none'
+// })
