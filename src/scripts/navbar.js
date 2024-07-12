caa2294bebf6882b9c40ae210786f8334dbdb9fc
daydreamer <nimandrey22@gmail.com>
2024-07-12 10:58:24 +0300

    1

--- src/scripts/navbar.js
+++ src/scripts/navbar.js
@@ -1,6 +1,14 @@
 const burgerBtn = document.querySelector('#burgerBtn');
 const navBurger = document.querySelector('#navBurger');
 
-burgerBtn.addEventListener('click', () => {
+burgerBtn.addEventListener('click', (e) => {
     navBurger.classList.toggle('active')
+})
+
+console.log(navBurger.getBoundingClientRect());
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 1) {
+        navBurger.classList.remove('active')
+    }
 })
